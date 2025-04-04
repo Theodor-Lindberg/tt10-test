7a3a7eeb647a0be99e9b819f9527b38b58bfe7ce
Theodor Lindberg <theodor.lindberg@liu.se>
2025-04-04 13:38:33 +0200

    Fix USERNAME in setup.h

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ module load prog/oss-cad-suite/2025.03.10
 module load prog/anaconda3/2024.02.1
 
 # The Python installation from anaconda conflicts with oss-cad-suite, this fixes that
-export PYTHONPATH=/home/$(USERNAME)/.local/lib/python3.11/site-packages/
+export PYTHONPATH=/home/$USERNAME/.local/lib/python3.11/site-packages/
 
 # Install cocotb for running the testbench
 python -m pip install cocotb
